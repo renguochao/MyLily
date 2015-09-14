83c5931243f0d2cf94856669ace1e20df0fc6855
Guochao Ren <314017427@qq.com>
2015-09-14 23:13:19 +0800

    添加服务器相关URL

--- MyLily/Classes/Other/Common.h
+++ MyLily/Classes/Other/Common.h
@@ -9,7 +9,10 @@
 #ifndef MyLily_Common_h
 #define MyLily_Common_h
 
-// 1.判断是否为iOS7
+// 1. 判断是否为iOS7
 #define iOS7 ([[UIDevice currentDevice].systemVersion doubleValue] >= 7.0)
 
+// 2. 服务器URL
+#define BASEURL @"http://bbs.nju.edu.cn/"
+#define TOP10URL @"bbstop10"
 #endif
